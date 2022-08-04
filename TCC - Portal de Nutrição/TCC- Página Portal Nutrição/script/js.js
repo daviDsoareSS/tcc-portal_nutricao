fe7b65df280e721d3969da09c549ee5d4b9d7e6d
unknown <david.silva425@etec.sp.gov.br>
2022-08-04 14:41:12 -0300

    calculadoraIMC atualizada

--- TCC - Portal de Nutrição/TCC- Página Portal Nutrição/script/js.js
+++ TCC - Portal de Nutrição/TCC- Página Portal Nutrição/script/js.js
@@ -1,14 +1,34 @@
-function calcularIMC(){
-    const pesoUser = document.getElementById('#peso');
-    const alturaUser = document.getElementById('#altura');
-    const peso = pesoUser.value;
-    const altura = pesoUser.value;
+    const btnCalcular = document.querySelector("#btn-calcular")
 
-    const multiplicacao = altura * altura;
-    const divisao = peso / multiplicacao;
-
-    const resultado = (divisao);
+    btnCalcular.addEventListener("click",function(e){
+        e.preventDefault();
+        const peso = document.querySelector("#peso");
+        const pesoUser = peso.value;
+        const altura = document.querySelector("#altura");
+        const alturaUser = altura.value;
     
-    console.log(resultado);
+        const multiplicacao = alturaUser * alturaUser;
+        const divisao = pesoUser / multiplicacao;
+    
+        const resultado = (divisao);
+        
+        document.getElementById("resultadoIMC").innerHTML = "Resultado: "+resultado.toFixed(2);   
+
+        if (resultado < 17) {
+            document.getElementById("classificacaoIMC").innerHTML = "</p>Sua classificação: Muito abaixo do peso</p>";
+        } else if (resultado < 18.49 && resultado > 17) {
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Abaixo do peso</p>";
+        } else if (resultado < 24.99 && resultado > 18.5){
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Peso normal</p>";
+        } else if (resultado < 29.99 && resultado > 25) {
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Acima do peso</p>";
+        } else if (resultado < 34.99 && resultado > 30) {
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Obesidade I</p>";
+        } else if (resultado < 39.99 && resultado > 35) {
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Obesidade II (severa)</p>";
+        } else if (resultado > 40){
+            document.getElementById("classificacaoIMC").innerHTML ="</p>Sua classificação: Obesidade II (severa)</p>";
+        } 
+    });
+
   
-}
