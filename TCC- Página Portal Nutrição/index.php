b7597a735a0d9973ca0b57e9760024f372db1d1a
daviDsoareSS <david.silva425@etec.sp.gov.br>
2022-08-25 13:06:53 -0300

    páginas reorganizadas

--- TCC- Página Portal Nutrição/index.php
+++ TCC- Página Portal Nutrição/index.php
@@ -2,7 +2,6 @@
   include('protect.php');
 ?>
 
-<<<<<<< HEAD
 <?php
   //HEADER
   include 'includes/header.php';
@@ -24,90 +23,6 @@
       </div>
   </div>
   <!-- fim do preloader --> 
-=======
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NutriAção -Início</title>
-    <link rel="icon" type="image/png" sizes="16x16"  href="img/favicon/favicon-16x16.png">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    <link rel="stylesheet" href="style.css">
-        <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Inter:wght@700&family=Lato&display=swap');
-    </style>
-</head>
-<body>
-
-  <!-- início do preloader -->
-  <div id="preloader">
-    <div class="inner">
-       <!-- HTML DA ANIMAÇÃO MUITO LOUCA DO SEU PRELOADER! -->
-       <img src="1474.gif" alt="" width="90px">
-       <div class="bolas">
-          <div></div>
-          <div></div>
-          <div></div>                    
-       </div>
-    </div>
-</div>
-<!-- fim do preloader --> 
-
-
-</body>
-<header>
-    <div class="container-header">
-      <a href="index.php"><img src="img/logo/NutriAçãoLogo.png" alt="" width="170px" height="100px"></a>
-        <div class="user-login">
-            <a href="perfil.php"><small><?php echo($_SESSION['nome']);?></small></a>
-            <img src="img/header/user-logo.png" width="60px" alt="">
-              <a href="logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 10v-5l8 7-8 7v-5h-8v-4h8zm-16-8v20h14v-2h-12v-16h12v-2h-14z"/></svg></a>
-        </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-    </div>
-</header>
-<hr>
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
-    <a class="navbar-brand" href="index.php">HOME</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">SOBRE</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="noticias.html">NOTÍCIAS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">DIETAS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">PLANOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="calculo.html">CÁLCULO IMC</a>
-          </li>
-        
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg></button>
-      </form>
-    </div>
-  </nav>
->>>>>>> b2d9855cbff23e360ab687fb5ca802e45000c81b
 <main>
   <div class="conteudo">
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
@@ -309,7 +224,6 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 <script src="script/js.js"></script>
 <script src="preloader.js"></script>
- 
->>>>>>> b2d9855cbff23e360ab687fb5ca802e45000c81b
+
 </body>
 </html>
--- TCC- Página Portal Nutrição/perfil.php
+++ TCC- Página Portal Nutrição/perfil.php
@@ -5,7 +5,6 @@
     datetime("d/m/Y", stretotime($dado["dataNasc"]))
 */
 ?>
-<<<<<<< HEAD
 <title>Meu perfil -Nutriação</title>
 
 <?php
@@ -26,85 +25,6 @@
       </div>
   </div>
   <!-- fim do preloader --> 
-=======
-<!DOCTYPE html>
-<html lang="pt-br"> 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portal de nutrição</title>
-    <link rel="icon" type="image/png" sizes="16x16"  href="img/favicon/favicon-16x16.png">
-    <meta name="msapplication-TileColor" content="#ffffff">
-    <meta name="theme-color" content="#ffffff">
-    <link rel="stylesheet" href="style.css">
-        <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <style>
-      @import url('https://fonts.googleapis.com/css2?family=Hammersmith+One&family=Inter:wght@700&family=Lato&family=Nunito+Sans&display=swap');
-    </style>
-</head>
-<body>
-  <!-- início do preloader -->
-  <div id="preloader">
-    <div class="inner">
-       <!-- HTML DA ANIMAÇÃO MUITO LOUCA DO SEU PRELOADER! -->
-       <div class="bolas">
-          <div></div>
-          <div></div>
-          <div></div>                    
-       </div>
-    </div>
-</div>
-<!-- fim do preloader --> 
-<header>
-    <div class="container-header">
-      <a href="index.php"><img src="img/logo/NutriAçãoLogo.png" alt="" width="170px" height="100px"></a>
-        <div class="user-login">
-            <a href="perfil.php"><small><?php echo($_SESSION['nome']);?></small></a>
-            <img src="img/header/user-logo.png" width="60px" alt="">
-            <a href="logout.php"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M16 10v-5l8 7-8 7v-5h-8v-4h8zm-16-8v20h14v-2h-12v-16h12v-2h-14z"/></svg></a>
-          </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-    </div>
-</header>
-<hr>
-<nav class="navbar navbar-expand-lg navbar-light bg-white">
-    <a class="navbar-brand" href="index.php">HOME</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-  
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="#">SOBRE</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="noticias.html">NOTÍCIAS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">DIETAS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">PLANOS</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="calculo.html">CÁLCULO IMC</a>
-          </li>
-        
-      </ul>
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg></button>
-      </form>
-    </div>
-  </nav>
->>>>>>> b2d9855cbff23e360ab687fb5ca802e45000c81b
   <hr>
 <main class="container-perfil">
     <div class="card-opcoes">
@@ -138,11 +58,7 @@
                   $dataNascimento = $_SESSION['dataNasc'];
                   $data = new DateTime($dataNascimento);
                   $resultado = $data->diff(new DateTime( date('Y-m-d')));
-<<<<<<< HEAD
                 ?>  
-=======
-                ?>
->>>>>>> b2d9855cbff23e360ab687fb5ca802e45000c81b
                 <strong><h5>Idade:</strong> <small><?php echo $resultado->format( '%Y anos' );?></small></h5>
                 <hr>
                 <strong><h5>Peso inicial:</strong> <small><?php echo($_SESSION['pesoInicial']);?></small></h5>
@@ -164,7 +80,6 @@
               </div>
             </div    
           </div>
-
 </main>
 </body>
 </html>
